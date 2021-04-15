b8bbbceb72f4dc62d54b29e0df52c2ab6f972002
serendipity <2274734747@qq.com>
2021-04-15 22:15:13 +0800

    leetCode

---
+++ java8-sharing/src/main/java/com/serendipity/java8/LeetCode1441.java
@@ -0,0 +1,32 @@
+package com.serendipity.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.IntStream;
+
+/**
+ * @author serendipity
+ * @version 1.0
+ * @date 2021/4/15 21:45
+ */
+public class LeetCode1441 {
+    public static List<String> buildArray(int[] target, int n) {
+        List<String> list = new ArrayList<>();
+        for(int i = 1,j=0;i <= n&&j<target.length;i++){
+            if(i<target[j]){
+                list.add("Push");
+                list.add("Pop");
+            }
+            if(i == target[j]){
+                list.add("Push");
+                j++;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(buildArray(new int[]{ 2,3,4},4));
+    }
+}
---
+++ java8-sharing/src/main/java/com/serendipity/java8/LeetCode1598.java
@@ -0,0 +1,32 @@
+package com.serendipity.java8;
+
+import java.util.Stack;
+
+/**
+ * @author serendipity
+ * @version 1.0
+ * @date 2021/4/15 21:23
+ */
+public class LeetCode1598 {
+    public static int minOperations(String[] logs) {
+        Stack<Integer> stack = new Stack<>();
+        for(String str :logs){
+            if(str.equals("./")){
+                continue;
+            }
+            else if(str.equals("../")){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }else {
+                stack.push(1);
+            }
+        }
+        return stack.size();
+    }
+
+    public static void main(String[] args) {
+       String[] logs = {"d1/","../","../","../"};
+        System.out.println(minOperations(logs));
+    }
+}
