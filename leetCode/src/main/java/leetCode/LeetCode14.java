5615552ee2cf7204c2cd806e88b2d37eaa605fb1
serendipity <2274734747@qq.com>
2021-05-11 21:23:20 +0800

    feat:leetCode

---
+++ leetCode/src/main/java/leetCode/LeetCode14.java
@@ -0,0 +1,30 @@
+package leetCode;
+
+/**
+ * @author serendipity
+ * @version 1.0
+ * @date 2021/5/11 20:57
+ */
+public class LeetCode14 {
+
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs==null){
+            return "";
+        }
+        StringBuffer sb = new StringBuffer(strs[0]);
+
+
+        fl
+
+        for (int j = 1;j<strs.length;j++){
+            while (!strs[j].startsWith(sb.toString())){
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"flower","flow","flight"}));
+    }
+}
