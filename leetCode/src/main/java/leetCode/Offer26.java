96288494f1487cb1c17d50a4f2f99ab2325076ce
serendipity <2274734747@qq.com>
2021-07-19 22:17:11 +0800

    leetCode

---
+++ leetCode/src/main/java/leetCode/Offer26.java
@@ -0,0 +1,32 @@
+package leetCode;
+
+/**
+ * @author serendipity
+ * @version 1.0
+ * @date 2021/7/19 22:14
+ */
+public class Offer26 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+
+    boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) {
+            return true;
+        }
+        if (A == null || A.val != B.val) {
+            return false;
+        }
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+}
